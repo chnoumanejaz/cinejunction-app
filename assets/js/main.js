ad54829d58954ee3e6dd5f3808845081d5b82f37
M. Nouman Ejaz <noumanejaz92@gmail.com>
2023-07-29 01:36:40 +0500

    home page completed - chatbot design + icons added

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,7 @@ const resetQuestions = function () {
     if (q.classList.contains('section__visible--active')) {
       q.classList.remove('section__visible--active');
       resetFaqIcon.forEach(ico => {
-        ico.textContent = '+';
+        ico.style.transform = 'rotate(0deg)';
       });
     }
   });
@@ -65,6 +65,49 @@ faqSection.addEventListener('click', function (e) {
   else {
     resetQuestions();
     question.classList.add('section__visible--active');
-    faqIcon.textContent = '-';
+    faqIcon.style.transform = 'rotate(180deg) translateY(1rem)';
+  }
+});
+
+// Added to watch List
+const addBtn = document.querySelectorAll('.addbtn');
+
+addBtn.forEach(btn => {
+  btn.addEventListener('click', function () {
+    if (btn.innerHTML.trim() === 'Added to Watch List') {
+      btn.innerHTML = 'Removed from Watch List';
+      setTimeout(() => {
+        btn.innerHTML = `<svg class="svg svg-btnset">
+                                <use xlink:href="./assets/img/sprite.svg#icon-plus"></use>
+                           </svg>`;
+        btn.style.borderRadius = '50%';
+      }, 1500);
+    } else {
+      btn.innerHTML = 'Added to Watch List';
+      btn.style.width = 'fit-content';
+      btn.style.padding = '.5rem 1rem';
+      btn.style.borderRadius = '5px';
+      btn.style.color = '#fff';
+    }
+  });
+});
+
+// Theme Management in the navbar
+const themeBtn = document.querySelector('.nav-theme');
+const body = document.querySelector('body');
+
+const themeChanger = function (icon) {
+  return `
+        <svg class="svg">
+            <use xlink:href="./assets/img/sprite.svg#icon-${icon}"></use>
+        </svg>`;
+};
+themeBtn.addEventListener('click', function () {
+  if (themeBtn.innerHTML === themeChanger('moon')) {
+    body.classList.add('sun');
+    themeBtn.innerHTML = themeChanger('sun');
+  } else {
+    themeBtn.innerHTML = themeChanger('moon');
+    body.classList.remove('sun');
   }
 });
